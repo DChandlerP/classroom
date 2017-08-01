ffaafa216bbf6e797d23de936ca79692340bdf01
dchandlerp <chandler.prince@gmail.com>
2017-08-01 06:06:27 -0500

    Added Controller for Add.html.

--- src/main/java/com/prince/classroom/controllers/StudentController.java
+++ src/main/java/com/prince/classroom/controllers/StudentController.java
@@ -1,24 +1,38 @@
 package com.prince.classroom.controllers;
 
 import com.prince.classroom.data.StudentDAO;
+import com.prince.classroom.models.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by davidprince on 7/31/17.
  */
 
 @Controller
+@RequestMapping("student")
 public class StudentController {
 
     @Autowired
     StudentDAO studentDAO;
 
+
+    @RequestMapping(value = "")
     public String index (Model model){
         model.addAttribute("students", studentDAO.findAll());
         model.addAttribute("title", "List Of All Students");
 
-        return "index";
+        return "student/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddStudentForm(Model model) {
+        model.addAttribute("title", "Add Student");
+        model.addAttribute(new Student());
+        model.addAttribute("studeents", studentDAO.findAll());
+        return "student/add";
     }
 }
