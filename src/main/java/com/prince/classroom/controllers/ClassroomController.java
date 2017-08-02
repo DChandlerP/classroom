3ea14e049a070882684e332711a29b00ad8f277e
dchandlerp <chandler.prince@gmail.com>
2017-08-02 10:30:28 -0500

    Finished Classroom Controller

--- src/main/java/com/prince/classroom/controllers/ClassroomController.java
+++ src/main/java/com/prince/classroom/controllers/ClassroomController.java
@@ -1,15 +1,19 @@
 package com.prince.classroom.controllers;
 
 import com.prince.classroom.data.ClassroomDAO;
+import com.prince.classroom.models.Classroom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.Valid;
 
 /**
  * Created by davidprince on 8/1/17.
@@ -26,5 +30,24 @@ public class ClassroomController {
         model.addAttribute("title", "Classrooms");
         model.addAttribute("classrooms", classroomDAO.findAll());
         return "classroom/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Classroom());
+        model.addAttribute("title", "Add Classroom");
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Classroom classroom, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Classroom");
+            return "category/add";
+        }
+
+        classroomDAO.save(classroom);
+        return "redirect:";
     }
 }
