6e147dc0f2572d5ced34e3e693531d621b47c53b
dchandlerp <chandler.prince@gmail.com>
2017-08-02 12:57:39 -0500

    Fixed errors where classroom was displaying an object rather than a property.

--- src/main/java/com/prince/classroom/models/Student.java
+++ src/main/java/com/prince/classroom/models/Student.java
@@ -43,7 +43,7 @@ public class Student {
     public String toString() {
         return  firstName  + " " +
                 lastName + " | " + "Class: " +
-                classroom + " | "+ "Student ID: " +
+                classroom.getName() + " | "+ "Student ID: " +
                 studentID ;
     }
 
