9f73690ea7276aac7d1c8d48bdb00dc1363835c1
dchandlerp <chandler.prince@gmail.com>
2017-08-02 17:41:40 -0500

    Created some Query methods in the StudentDAO. Haven't figured out how to use them.

--- src/main/java/com/prince/classroom/data/StudentDAO.java
+++ src/main/java/com/prince/classroom/data/StudentDAO.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by davidprince on 7/31/17.
@@ -12,4 +13,10 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public interface StudentDAO extends CrudRepository<Student, Integer> {
+    List<Student> findByStudentLastNameIgnoreCase(String lastName);
+    List<Student> findByStudentFirstNameIgnoreCase(String firstName);
+    List<Student> findByStudentStudentID(String studentID);
+    List<Student> findByStudentFirstNameAndLastNameIgnoreCase(String firstName, String lastName);
+
+    //Could Append Like for Names
 }
